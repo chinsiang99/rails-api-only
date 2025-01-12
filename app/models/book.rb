662e4f54d65dc118ce67b7c22d9b459be994569d
Tan Chin Siang <chinsiang99@gmail.com>
2025-01-12 19:49:46 +0800

    feat: adding minimum length validation

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,4 @@
 class Book < ApplicationRecord
-  validates :title, presence: true, on: :create
-  validates :author, presence: true, on: :create
+  validates :title, presence: true, length: { minimum: 3 }, on: :create
+  validates :author, presence: true, length: { minimum: 3 }, on: :create
 end
