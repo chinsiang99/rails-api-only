681f9513a4d696ba4f5f8dd8624eaa49e413bf6d
Tan Chin Siang <chinsiang99@gmail.com>
2025-01-12 18:29:08 +0800

    feat: create routes books GET api

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,14 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  # get "/books" => "books#index"
+  namespace :api do
+    namespace :v1 do
+      # resources :books, only: [ :index, :show, :create, :update, :destroy ]
+      resources :books, only: [ :index ]
+    end
+  end
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
