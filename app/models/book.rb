82adb7e556830f19c47ac530741bfd8b272a96da
Tan Chin Siang <chinsiang99@gmail.com>
2025-01-13 23:46:41 +0800

    feat: adding associations in model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ApplicationRecord
   validates :title, presence: true, length: { minimum: 3 }, on: :create
-  validates :author, presence: true, length: { minimum: 3 }, on: :create
+  # validates :author, presence: true, length: { minimum: 3 }, on: :create
+
+  belongs_to :author
 end
