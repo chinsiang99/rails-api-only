95f196a000a2ae32bc79feee31f7eeaf47205a77
Tan Chin Siang <chinsiang99@gmail.com>
2025-01-14 00:26:19 +0800

    feat: working with complex mapping

--- app/representers/books_representer.rb
+++ app/representers/books_representer.rb
@@ -8,8 +8,9 @@ class BooksRepresenter
         {
           id: book.id,
           title: book.title,
-          author_first_name: book.author&.first_name,
-          author_last_name: book.author&.last_name,
+          author_first_name: book.author&.first_name.to_s, # use .to_s will turn nil into empty string
+          author_last_name: book.author&.last_name.to_s, # we have &. to indicate if doesnt have author then it will return nil
+          author_name: "#{book.author&.first_name} #{book.author&.last_name}",
           author_age: book.author&.age
         }
       end
